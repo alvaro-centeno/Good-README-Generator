b30e7090810a396a14383eb8357712d3f6c3bbd0
Alvaro Centeno <alvaroc0689@gmail.com>
2020-05-31 00:52:18 -0700

    added prompt questions and axios return of user git

--- index.js
+++ index.js
@@ -1,12 +1,80 @@
-const questions = [
+const fs = require('fs');
+const inquirer = require('inquirer');
+const axios = require('axios');
 
-];
+inquirer.prompt({
+    messgae: "Enter Github username"
+}).then(function ({ username }) {
+    const searchUrl = `https://api.github.com/users/${username}`
 
-function writeToFile(fileName, data) {
-}
+    axios.get(searchUrl).then((res) =>
+        fs.writeFile('README.md', { username }), function (err) {
+            if (err) {
+                console.log(err);
 
-function init() {
+            } else {
+                console.log('success');
 
-}
+            }
+        });
+});
 
-init();
+inquirer.prompt([
+    {
+        type: "input",
+        message: "what is your name",
+        name: "username"
+
+    },
+    {
+        type: "input",
+        message: "Project title:",
+        name: "title"
+
+    },
+    {
+        type: "input",
+        message: "Project description:",
+        name: "description"
+
+    },
+    {
+        type: "input",
+        message: "Table of contents",
+        name: "table"
+
+    },
+    {
+        type: "input",
+        message: "Installation",
+        name: "installation"
+
+    },
+    {
+        type: "input",
+        message: "Usage",
+        name: "Usage"
+
+    },
+    {
+        type: "input",
+        message: "Usage",
+        name: "Usage"
+
+    }, {
+        type: "input",
+        message: "License",
+        name: "license"
+
+    }, {
+        type: "input",
+        message: "Contributing",
+        name: "contributing"
+
+    }, {
+        type: "input",
+        message: "Tests",
+        name: "tests"
+
+    },
+])
