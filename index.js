417323a930e5aa705d826b444d35debcf9ec73cd
Alvaro Centeno <alvaroc0689@gmail.com>
2020-05-31 01:29:13 -0700

    all questions prompt. Read me written at end

--- index.js
+++ index.js
@@ -2,29 +2,10 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const axios = require('axios');
 
-inquirer.prompt({
-    type: 'input',
-    messgae: "Enter Github username",
-    name: 'username'
-}).then(function ({ username }) {
-    const searchUrl = `https://api.github.com/users/${username}`;
-
-    axios.get(searchUrl).then((res) =>
-        fs.writeFile('README.md', { username }), function (err) {
-            if (err) {
-                console.log(err);
-
-            } else {
-                console.log('success');
-
-            }
-        });
-});
-
 inquirer.prompt([
     {
         type: "input",
-        message: "what is your git hub username",
+        message: "what is your github username",
         name: "user"
 
     },
@@ -38,12 +19,6 @@ inquirer.prompt([
         type: "input",
         message: "Project description:",
         name: "description"
-
-    },
-    {
-        type: "input",
-        message: "Table of contents",
-        name: "table"
 
     },
     {
@@ -78,21 +53,34 @@ inquirer.prompt([
     console.log(res.user);
     console.log(res.title);
     console.log(res.description);
-    console.log(res.table);
     console.log(res.installation);
     console.log(res.usage);
     console.log(res.license);
     console.log(res.contributing);
     console.log(res.tests);
+    fs.writeFile('README.md', `#Title: ${res.title}
+    #Description ${res.description}
+    #Installation ${res.installation}
+    #Tables of Contents: 
+    - Description
+    - Table of Contents
+    - Installation
+    - Usage
+    - License
+    - Contributing
+    - Tests
+    #Installation ${res.installation}
+    #Usage ${res.usage}
+    #License ${res.usage}
+    #Contributing ${res.contributing}
+    #Tests ${res.tests}`, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('ReadMe created!');
 
+        }
 
-
-
-
-
-
-
-
-
+    })
 
 })
